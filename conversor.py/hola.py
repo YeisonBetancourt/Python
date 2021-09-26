8521fa67ea401528ea1699bcfddf5ae20653059c
Yeison Betancourt <yefegobe01@gmail.com>
2021-09-26 06:44:50 -0500

    Un peuqeno programa, para paracticar lo aprendido el dia de ayer

---
+++ conversor.py/hola.py
@@ -0,0 +1,17 @@
+hola = """
+Hola, muy beunso dias a todos y a todas, hoy estoy super feliz, mas feliz que nunca por que desde hoy, me enfoco nuevamente en mis objetivos, en aprender ingles y programacion, ademas de lograr los abs de un dios griego...
+
+
+    
+Estas realamente comprometido? si o no? 
+"""
+
+opcion = input(hola)
+
+if opcion == ("si"):
+    print ("vamos crack tu puedes")
+elif opcion == ("no"):
+    print ("En serio, esperaba mas de ti, no penese que fueras tan debil")
+else: 
+    print("Elige gilipollas, si o no")
+
